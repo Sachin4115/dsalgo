a8974133d1ec574e9281be5ffbcfcb2d0e29ab58
Sachin4115 <sachdeva.4115@gmail.com>
2022-11-20 10:12:00 +0530

    Loop day 4

---
+++ Loop/Binomial_pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+            printf("  ");
+        }
+        printf("   1");
+        for(int j=1;j<=i-1;j++){
+            int fact1=1,fact2=1,fact3=1;
+            for(int k=i-1;k>=1;k--){
+                fact1*=k;
+            }
+            for(int k=j;k>=1;k--){
+                fact2*=k;
+            }
+            for(int k=i-1-j;k>=1;k--){
+                 fact3*=k;
+            }
+            printf("   %d",fact1/(fact2*fact3));
+        }
+        printf("\n");
+    }
+    return 0;
+}
