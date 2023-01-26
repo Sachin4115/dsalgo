242da387376099a2c0ae897991a38fff93ec498c
Sachin4115 <sachdeva.4115@gmail.com>
2023-01-26 11:11:23 +0530

    Bubble_Sort

---
+++ Sort/Bubble_sort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,arr[5]={3,4,2,5,1};
+    // for(i=0;i<5;i++){
+    //     printf("%d ",arr[i]);
+    // }
+    printf("%u",arr[]);
+    return 0;
+}
