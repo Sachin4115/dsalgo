aa920e9c146ef326b7ffe529f13a2fa1df6243a7
Sachin4115 <sachdeva.4115@gmail.com>
2023-07-15 16:35:21 +0530

    Sort

--- Sort/Bubble_sort.c
+++ Sort/Bubble_sort.c
@@ -2,10 +2,18 @@
 
 int main()
 {
-    int i,j,arr[5]={3,4,2,5,1};
-    // for(i=0;i<5;i++){
-    //     printf("%d ",arr[i]);
-    // }
-    printf("%u",arr[]);
+    int i,j,arr[5]={3,4,2,5,1},temp;
+    for(i=0;i<5;i++){
+        printf("%d ",arr[i]);
+    }
+    for(i=0;i<5;i++){
+        for(j=0;j<4-i;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
     return 0;
 }
---
+++ Sort/Quick_Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int partition(int arr[],int start,int end){
+    int pivot=arr[end];
+    int PI=start;
+    for(int i=start;i<end;i++){
+        if(arr[i]<=pivot){
+            swap(arr[i],arr[PI]);
+            PI++;
+        }
+    }
+    swap(arr[PI],arr[end]);
+    return PI;
+}
+void quicksort(int arr[],int end,int start){
+    if(start>=end)
+        return;
+    int pivot = partition(arr,start,end);
+    quicksort(arr,pivot-1,start);
+    quicksort(arr,end,pivot+1);
+}
+int main()
+{
+    int arr[]={9,-3,5,2,6,8,-6,1,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr,n-1,0);
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
