a8b00fd887f4645b8a8999c33c52d76a0b4616e8
Sachin4115 <sachdeva.4115@gmail.com>
2023-08-28 19:54:02 +0530

    Recursion

---
+++ Recursion/Recur_Basic/Print_Decreasing_Increasing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void print_decreasing(int n){
+    if(n==0)
+        return;
+    cout<<n<<endl;
+    print_decreasing(n-1);
+    cout<<n<<endl;
+}
+int main()
+{
+    int n=5;
+    print_decreasing(n);
+    return 0;
+}
