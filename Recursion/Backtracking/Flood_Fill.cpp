506ee6c7264da2d6c6a13512c1b944e135683763
Sachin4115 <sachdeva.4115@gmail.com>
2023-09-01 03:30:35 +0530

    Recursion

---
+++ Recursion/Backtracking/Flood_Fill.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void floodfill(vector<vector<int>> maze, int sr, int sc, string asf) {
+    if((sr==maze.size())||(sc==maze[0].size())||(sr<0)||(sc<0)||maze[sr][sc]==1){
+        return;
+    }
+    if((sr==maze.size()-1)&&(sc==maze[0].size()-1)){
+        cout<<asf<<endl;
+        return;
+    }
+        maze[sr][sc]=1;
+        floodfill(maze,sr-1,sc,asf+'t');
+        floodfill(maze,sr,sc-1,asf+'l');
+        floodfill(maze,sr+1,sc,asf+'d');
+        floodfill(maze,sr,sc+1,asf+'r');
+        maze[sr][sc]=0;
+}
+
+int main() {
+  int n, m;
+  cin >> n ;
+  cin >> m;
+  vector < vector < int >> arr(n, vector < int > (m));
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      cin >> arr[i][j];
+
+  floodfill(arr, 0, 0, "");
+}
