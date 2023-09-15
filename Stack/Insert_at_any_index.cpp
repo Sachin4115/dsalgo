27126eb0cfa8a9023ab95a2c8a5bfd92552c9ab4
Sachin4115 <sachdeva.4115@gmail.com>
2023-09-15 16:46:01 +0530

    Stack

---
+++ Stack/Insert_at_any_index.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertAtB(stack<int> &st,int ele){
+    if(st.empty()){
+        st.push(ele);
+        return;
+    }
+    int t=st.top();
+    st.pop();
+    insertAtB(st,ele);
+    st.push(t);
+}
+void insert(stack<int> &st,int n,int ele){
+    if(n==st.size()){
+        st.push(ele);
+        return;
+    }
+    else{
+        int t=st.top();
+        st.pop();
+        insert(st,n,ele);
+        st.push(t);
+    }
+}
+
+int main()
+{
+    stack <int> st;
+    st.push(1);
+    st.push(2);
+    st.push(4);
+    st.push(5);
+    // while(!st.empty()){
+    //     cout<<st.top()<<endl;
+    //     st.pop();
+    // }
+    // insertAtB(st,3);
+    // while(!st.empty()){
+    //     cout<<st.top()<<endl;
+    //     st.pop();
+    // }
+    insert(st,2,3);
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    return 0;
+}
