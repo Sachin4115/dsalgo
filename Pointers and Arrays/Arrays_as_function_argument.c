fc84f37116a98bd51591f08c6371fe0a3115ca20
Sachin4115 <sachdeva.4115@gmail.com>
2024-05-05 08:23:46 +0530

    Array

--- Pointers and Arrays/Arrays_as_function_argument.c
+++ Pointers and Arrays/Arrays_as_function_argument.c
@@ -5,7 +5,7 @@ void printnumbers(int arr[],int n);//Array as array.
 int main()
 {
     int arr[]={1,2,3,4,5,6,7,8,9,10};
-    printnumbers(arr,10);
+    Printnumbers(arr,10);
     printf("\n");
     printnumbers(arr,10);
     return 0;
