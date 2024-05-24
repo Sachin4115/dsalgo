35949805424e898830508ce641d568a983e778b4
Sachin4115 <sachdeva.4115@gmail.com>
2024-05-24 12:28:52 +0530

    Tree

--- Tree/Generic_Tree/Iterative_Traversal.cpp
+++ Tree/Generic_Tree/Iterative_Traversal.cpp
@@ -53,6 +53,35 @@ void preOrder(node* root){
     }
 }
 
+void preAndPostOpt(node* root){
+    vector<int> pre;
+    vector<int> post;
+    pair<node*,int> p;
+    stack<pair<node*,int>> st;
+    p.first=root;
+    p.second=-1;
+    st.push(p);
+    while(!st.empty()){
+        if(st.top().second==-1){
+            pre.push_back(st.top().first->data);
+            st.top().second++;
+        }else if(st.top().second<st.top().first->children.size()){
+            st.push({st.top().first->children[st.top().second++],-1});
+        }else{
+            post.push_back(st.top().first->data);
+            st.pop();
+        }
+    }
+    for(int i:pre){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(int i:post){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
 // void postOrder(node* root){
 //     stack<node*>st;
 //     st.push(root);
@@ -64,13 +93,12 @@ void preOrder(node* root){
 int main()
 {
 
-    vector<int> input = {1,2,3,-1,-1,4,5,-1,6,7,-1,-1,9,-1,-1,10,11,-1,12,-1,-1,-1};
+    vector<int> input = {1,2,3,-1,-1,4,5,-1,6,7,-1,8,-1,-1,9,-1,-1,10,11,-1,12,-1,-1,-1};
     node* root = createTree(input);
 
     preOrder(root);
     cout<<endl;
-    postOrder(root);
-    cout<<endl;
+    preAndPostOpt(root);
 
     return 0;
 }
