1136ac3f55893275651bd002ccd3b13cc0559c20
Sachin4115 <sachdeva.4115@gmail.com>
2024-10-08 13:37:54 +0530

    dp

---
+++ DP/FriendsPairingProb.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    int dp[n+1];
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+((i-1)*dp[i-2]);
+    }
+    cout<<dp[n];
+    return 0;
+}
