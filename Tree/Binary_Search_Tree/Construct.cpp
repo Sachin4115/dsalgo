170f9fee52a9663eb6af939e5c92e3282027757c
Sachin4115 <sachdeva.4115@gmail.com>
2024-11-07 22:48:15 +0530

    BST

---
+++ Tree/Binary_Search_Tree/Construct.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct node{
+    int data;
+    node* left;
+    node* right;
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+node* construct(vector <int> &arr, int l, int h){
+    if(l>h)
+        return NULL;
+    int m = (l + h) / 2;
+    int d = arr[m];
+    node* lc = construct(arr,l,m-1);
+    node* rc = construct(arr,m+1,h);
+    node* t = new node(d);
+    t->left = lc;
+    t->right = rc;
+    return t;
+}
+
+void display(node* root){
+    if(root==NULL)
+        return;
+    string str;
+    str += root->left!=NULL?to_string(root->left->data)+" ->":".  ->";
+    str += to_string(root->data);
+    str += root->right!=NULL?"<- "+to_string(root->right->data):"<-  .";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+
+int main()
+{
+    vector<int> arr = {12 , 25 , 37 , 50 , 62 , 75 , 87};
+    node* root = construct(arr,0,arr.size()-1);
+    display(root);
+    return 0;
+}
