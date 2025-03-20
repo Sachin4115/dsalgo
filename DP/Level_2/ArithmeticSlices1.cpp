12e9bae3995f5b3d3e7e3110f9922b4f234ba49f
Sachin4115 <sachdeva.4115@gmail.com>
2025-03-20 14:16:56 +0530

    DP

--- DP/Level_2/ArithmeticSlices1.cpp
+++ DP/Level_2/ArithmeticSlices1.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int main()
 {
     vector<int> arr = {2,3,9,12,15,18,22,26,30,34,36,38,40,41};
-     
+    int n = arr.size();
+    int ans=0;
+    vector<int> dp(n,0);
+    for(int i=2;i<n;i++){
+        if(arr[i]-arr[i-1]==arr[i-1]-arr[i-2]) dp[i]=dp[i-1]+1;
+        ans+=dp[i];
+    }
+    cout<<ans;
     return 0;
 }
